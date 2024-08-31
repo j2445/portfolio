c7361e8b2075803ba5728d6aeb45b03e4f7e7960
JOY PATEL <121655607+j2445@users.noreply.github.com>
2024-08-31 13:59:50 +0530

    ok

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-document.documentElement.scrollTop = 0;
+const body = document.body
 
 $(window).on('beforeunload', function() {
   $('body').hide();
   $(window).scrollTop(0);
 });
 
-const body = document.body
 const btnTheme = document.querySelector('.fa-moon')
 const btnHamburger = document.querySelector('.fa-bars')
 
